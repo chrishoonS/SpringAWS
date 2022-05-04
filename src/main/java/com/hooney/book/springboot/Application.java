6199ad338efc7ea42bd83462615008b5ede000cf
chrishoon <chrishoon90@daum.net>
2022-05-04 10:02:49 +0900

    fix : edit import

--- src/main/java/com/hooney/book/springboot/Application.java
+++ src/main/java/com/hooney/book/springboot/Application.java
@@ -1,6 +1,5 @@
 package com.hooney.book.springboot;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
