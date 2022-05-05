09d6b6641e3c9103d8783991b79e7050182187a2
chrishoonS <chrishoon90@daum.net>
2022-05-05 22:55:59 +0900

    fix : 주석 추가 및 코드 수정

--- src/main/java/com/hooney/book/springboot/codeTest/Solution02.java
+++ src/main/java/com/hooney/book/springboot/codeTest/Solution02.java
@@ -1,12 +1,10 @@
 package com.hooney.book.springboot.codeTest;
 
 public class Solution02 {
-    public int solution(int[] a, int[] b) {
+    public int solution(int[] a, int[] b) { //내적
         int answer = 0;
-        int temp = 0;
         for(int i=0; i < a.length; i++){
-            temp = a[i]*b[i];
-            answer += temp;
+            answer += a[i]*b[i];             //a요소와 b요소 값을 순차적으로 곱하여, 순차적으로 더해나감
         }
 
         return answer;
