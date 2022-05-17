82723fa18fc4546d8ef16f44f7f24d009dde5e8a
chrishoon <chrishoon90@daum.net>
2022-05-17 13:05:25 +0900

    fix : 비트연산 예제 공부

--- src/main/java/com/hooney/book/springboot/codeTest/Rambda2.java
+++ src/main/java/com/hooney/book/springboot/codeTest/Rambda2.java
@@ -13,11 +13,21 @@ public class Rambda2 {
     }
 
     public static void main(String[] args) {
-        exec((i,j)->{
-            System.out.println("exec i :::::::::" + i);
-            System.out.println("exec j :::::::::" + j);
-            return i+j;
-        });
+        int b1 = 0B0010; // 0B -> 2진수 리터럴
+        int b2 = 0B0101;
+        int b3 = 0B1111;
+        int rtAndOp = b1 & b3; // 비트 AND 연산자
+        int rtOrOp = b1 | b2; // 비트 OR 연산자
+        int rtXorOp = b1 ^ b3; // 비트 XOR 연산자
+        System.out.println("b1 AND b3 : " + Integer.toBinaryString(rtAndOp)); // 0010
+        System.out.println("b1 OR b2 : " + Integer.toBinaryString(rtOrOp)); // 0111
+        System.out.println("b1 XOR b3 : " + Integer.toBinaryString(rtXorOp)); // 1101
+        System.out.println(Integer.toBinaryString(9));
+//        exec((i,j)->{
+//            System.out.println("exec i :::::::::" + i);
+//            System.out.println("exec j :::::::::" + j);
+//            return i+j;
+//        });
     }
 
 }
