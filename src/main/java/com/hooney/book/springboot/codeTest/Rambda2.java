a4c9572a0aba07d46093ae5571047507e4693d46
chrishoon <chrishoon90@daum.net>
2022-06-22 16:34:23 +0900

    fix : 날짜 연산 study

--- src/main/java/com/hooney/book/springboot/codeTest/Rambda2.java
+++ src/main/java/com/hooney/book/springboot/codeTest/Rambda2.java
@@ -1,5 +1,10 @@
 package com.hooney.book.springboot.codeTest;
 
+import org.apache.tomcat.jni.Local;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Rambda2 {
 
     //람다식 공부
@@ -13,21 +18,32 @@ public class Rambda2 {
     }
 
     public static void main(String[] args) {
-        int b1 = 0B0010; // 0B -> 2진수 리터럴
-        int b2 = 0B0101;
-        int b3 = 0B1111;
-        int rtAndOp = b1 & b3; // 비트 AND 연산자
-        int rtOrOp = b1 | b2; // 비트 OR 연산자
-        int rtXorOp = b1 ^ b3; // 비트 XOR 연산자
-        System.out.println("b1 AND b3 : " + Integer.toBinaryString(rtAndOp)); // 0010
-        System.out.println("b1 OR b2 : " + Integer.toBinaryString(rtOrOp)); // 0111
-        System.out.println("b1 XOR b3 : " + Integer.toBinaryString(rtXorOp)); // 1101
-        System.out.println(Integer.toBinaryString(9));
+//        System.out.println(Integer.toBinaryString(9));
 //        exec((i,j)->{
 //            System.out.println("exec i :::::::::" + i);
 //            System.out.println("exec j :::::::::" + j);
 //            return i+j;
 //        });
+
+        //오늘 날짜
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
+        //특정 날짜 사이의 연/월/일 차이
+        LocalDate targetDate1 = LocalDate.of(1990,11,3);
+        System.out.println(today.until(targetDate1));
+
+        //특정 날짜 사이의 차이를 일 수로
+        long d1 = ChronoUnit.DAYS.between(today, targetDate1);
+        long d2 = ChronoUnit.DAYS.between(today, LocalDate.of(2022, 8, 3));
+        System.out.println(d1);
+        System.out.println(d2);
+
+        //특정 날짜의 100일 전/후
+        System.out.println(ChronoUnit.DAYS.addTo(today, -100));
+        System.out.println(ChronoUnit.DAYS.addTo(today, 100));
+
+
     }
 
 }
